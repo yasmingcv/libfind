fbfd6ae0cfbbed44736b3fbfe8f7faff8618ed26
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-01-12 15:25:59 -0300

    Home web estático

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,21 @@
-import '../../style.css'
 import './style.css'
+import imghome from '../../assets/imghome.png'
+import { Link } from "react-router-dom"
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div>
-            home
+            <div className='home__info'>
+                <img src={imghome} alt='pic home' />
+                <div className='info__text'>
+                    <p>Explore os reinos infinitos da imaginação com <strong>LibFind</strong>, onde cada busca se torna uma jornada literária. Navegue por bibliotecas virtuais, desvende histórias entrelaçadas e descubra mundos inexplorados de conhecimento. Com uma busca, desvende as páginas de aventuras, romance e sabedoria. <strong>LibFind</strong> - O portal que transforma suas buscas em descobertas literárias extraordinárias.
+                    </p>
+
+
+                    <Link to='/buscar' className='button__text'>Comece agora</Link>
+
+                </div>
+            </div>
         </div>
     )
 }
