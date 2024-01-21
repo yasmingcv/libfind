b3bdbeacd2363d1f456b4238ff4d6d0add39f338
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-01-21 01:03:15 -0300

    .

--- src/components/CardBook/CardBook.jsx
+++ src/components/CardBook/CardBook.jsx
@@ -1,15 +1,29 @@
 import { Link } from "react-router-dom"
 import './style.css'
 
-function CardBook() {
+function CardBook({infoBook}) {
+
+    
+    //fetch
+
+    let capaaa = ''
+
+    if(!infoBook.imageLinks){
+        capaaa = 'https://www.shutterstock.com/image-vector/copy-vector-icon-illustration-sign-260nw-2047612412.jpg'
+    } else {
+        capaaa = infoBook.imageLinks.thumbnail
+    }
+
+    console.log('chegou', infoBook.title);
+
     return (
         <div className="card__book">
-            <img className='img__book' src="https://books.google.com/books/content?id=t_6VEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="capa"></img>
+            <img className='img__book' src={capaaa} alt="capa"></img>
 
             <div className="card__text">
                 <div>
-                    <h1>O diário de um banana 1</h1>
-                    <p>Jeff Kinney - Booksmile</p>
+                    <h1>{infoBook.title}</h1>
+                    <p>{infoBook.authors} - {infoBook.publisher}</p>
                 </div>
 
                 <Link to='/livro/id' className="button__details">Saiba mais</Link>
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,23 +1,62 @@
+import { useEffect, useState } from 'react'
 import CardBook from '../../components/CardBook/CardBook'
 import './style.css'
+import axios from 'axios'
 
 function Search() {
+
+    const [input, setInput] = useState([''])
+    const [dataBooks, setDataBooks] = useState([])
+
+    
+        const fetchSearchBooks = async (name) => {
+            console.log('entrorueroeo')
+            try {
+                const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${name}`)
+                console.log('response');
+                setDataBooks(response.data.items)
+
+            } catch (error) {
+                console.error('Erro ao buscar dados:', error)
+            }
+
+        }
+
+        // input.onChange(fetchSearchBooks(input))
+        //fetchSearchBooks('brasil')
+
+    
+
+
+   // console.log(dataBooks[0], input)
+
     return (
         <div className="search">
             <h2>Busque por títulos ou autores!</h2>
 
-            <input type="search" />
+            <input type="search" value={input} onChange={(e) => { setInput(e.target.value) }} />
 
             <div className="book__list"></div>
 
-            <span className='search__results'>
-                <CardBook />
-                <CardBook />
-                <CardBook />
-                <CardBook />
-                <CardBook />
+            <div className='search__results'>
                 
-            </span>
+                {
+                    dataBooks.map(book => {
+                        return(
+                            <CardBook key={book.id} infoBook={book.volumeInfo} />
+                        )
+                    })
+                }
+
+            </div>
+
+            {/* {dataBooks.map(book => {
+                return(
+                    <h3 color='white'>{book.volumeInfo.title}</h3>
+                )
+                
+            })} */}
+
 
         </div>
     )
